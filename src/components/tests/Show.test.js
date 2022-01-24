15a04cc32975e419165c60ce413bb305b7107d25
John S <johnc.sime@gmail.com>
2022-01-24 18:37:09 -0500

    Got the fourth test passing in Show.test.js

--- src/components/tests/Show.test.js
+++ src/components/tests/Show.test.js
@@ -4,6 +4,7 @@ import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
 import Show from './../Show';
+import Loading from '../Loading';
 
 const testShow = {
     name: 'test',
@@ -32,6 +33,12 @@ test('renders same number of options seasons are passed in', ()=>{
     expect(seasons).toHaveLength(3);
 });
 
-test('handleSelect is called when an season is selected', () => {});
+test('handleSelect is called when an season is selected', () => {
+    const handleSelect = jest.fn();
+    render(<Show show={testShow} selectedSeason={'none'} handleSelect={handleSelect}/>);
+    const season = screen.getByLabelText(/select a season/i);
+    userEvent.selectOptions(season, ['1']);
+    expect(handleSelect).toBeCalled();
+});
 
 test('component renders when no seasons are selected and when rerenders with a season passed in', () => {});
