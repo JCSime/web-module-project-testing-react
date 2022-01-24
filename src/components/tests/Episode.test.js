5fffa8b5fae2875c86054d612a05a111e348c125
John S <johnc.sime@gmail.com>
2022-01-24 16:40:55 -0500

    Got the first test in Episode.test.js to pass

--- src/components/tests/Episode.test.js
+++ src/components/tests/Episode.test.js
@@ -1,11 +1,35 @@
 import React from 'react';
 import '@testing-library/jest-dom/extend-expect';
 import { render, screen } from '@testing-library/react';
-import Episode from './../Episode';
+import Episode, { episode } from './../Episode';
+
+const testEpisode = {
+    id:1,
+    name: "",
+    image: "http://static.tvmaze.com/uploads/images/medium_landscape/67/168918.jpg",
+    season: 1,
+    number: 1,
+    summary: "",
+    runtime: 1
+}
+
+const testEpisodeWithoutImage = {
+    //Add in appropriate test data structure here.
+    id: 1,
+    name: '',
+    summary: 'test summary',
+    image: null
+}
 
 
-test("renders without error", () => {});
+test("renders without error", () => {
+    render(<Episode episode={testEpisode}/>);
+});
 
-test("renders the summary test passed as prop", ()=>{});
+test("renders the summary test passed as prop", ()=>{
 
-test("renders default image when image is not defined", ()=>{});
+});
+
+test("renders default image when image is not defined", ()=>{
+
+});
